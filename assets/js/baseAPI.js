5fea61f38267679621cd695e886a9cef835bb0d5
张zhao辉 <494272914@qq.com>
2020-02-29 09:39:15 +0800

    完成后台首页

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,12 +5,13 @@ $.ajaxPrefilter(function (option) {
         option.headers = {
             Authorization: localStorage.getItem('token')
         }
-    }
-
-    option.complete = function (res) {
-        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            localStorage.removeItem('token');
-            location.href = '/login.html'
+        option.complete = function (res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                localStorage.removeItem('token');
+                location.href = '/login.html'
+            }
         }
     }
+
+
 })
